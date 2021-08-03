4895c4fc353d9cbb98c9c4073fbe400069da7966
Alyssa Antonaccio <alyantonaccio@gmail.com>
2021-08-03 12:13:18 -0400

    Finished longestCommon

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -25,5 +25,31 @@ strs[i] consists of only lower-case English letters.
  * @return {string}
  */
  var longestCommonPrefix = function(strs) {
+  let result = '';
+  if (strs.length === 1) {
+          return strs[0];
+      }
+  let compare =  strs[0].split('');
+  const key = {};
 
+  for (let i = 0; i < compare.length; i++) {
+    result += compare[i];
+    key[result] = result;
+  }
+  result = '';
+  for (var j in key) {
+      let tracker = 0;
+    for (let i = 1; i < strs.length; i++) {
+      if (strs[i].slice(0, key[j].length) === key[j]) {
+        tracker += 1;
+      }
+    }
+    if (tracker === strs.length - 1) result = key[j]
+  }
+  return result;
 };
+
+console.log(longestCommonPrefix(["flower","flow","flight"]));
+console.log(longestCommonPrefix(["dog","racecar","car"]));
+console.log(longestCommonPrefix(["aaa","aa","aaa"]))
+console.log(longestCommonPrefix(["c","acc","ccc"]))
