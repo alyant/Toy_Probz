1f7770868d1f773494b4b7bbe153e9f3f4fe033d
Alyssa Antonaccio <alyantonaccio@gmail.com>
2021-08-04 19:13:17 -0400

    changed a bit

--- mergeSortedList.js
+++ mergeSortedList.js
@@ -35,8 +35,8 @@ Both l1 and l2 are sorted in non-decreasing order.
  */
  var mergeTwoLists = function(l1, l2) {
 
-  if (l1 == null) return l1;
-  if (l2 == null) return l2;
+  if (l1 === null) return l1;
+  if (l2 === null) return l2;
 
   if (l1.val < l2.val) {
      l1.next = mergeTwoLists(l1.next, l2);
