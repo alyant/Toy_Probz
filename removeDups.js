9ecfb4952c997487462fe52a0faa562a87255ebd
Alyssa Antonaccio <alyantonaccio@gmail.com>
2021-08-05 16:46:43 -0400

    Added solution

--- removeDups.js
+++ removeDups.js
@@ -47,5 +47,24 @@ nums is sorted in non-decreasing order.
  * @return {number}
  */
  var removeDuplicates = function(nums) {
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i - 1] === nums[i]) {
+      nums[i - 1] !== undefined ? nums[i - 1] = '-' : nums[i - 1] = '-';
+    } else {
+        count++;
+    }
+  }
+  nums.sort((a, b) => {
+      if (b === '-') {
+          return -1;
+      }
+      if (a === '-') {
+          return 1;
+      }
+  })
+  return count;
+}
 
- }
+ console.log(removeDuplicates([1,1,2]))
+ console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
