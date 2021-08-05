4271e8bd947e609c8edcd0877fe962e40c5442f7
Alyssa Antonaccio <alyantonaccio@gmail.com>
2021-08-05 16:11:49 -0400

    Added remove dupes

--- mergeSortedList.js
+++ mergeSortedList.js
@@ -34,9 +34,8 @@ Both l1 and l2 are sorted in non-decreasing order.
  * @return {ListNode}
  */
  var mergeTwoLists = function(l1, l2) {
-
-  if (l1 === null) return l1;
-  if (l2 === null) return l2;
+ if (l1 === null) return l2;
+  if (l2 === null) return l1;
 
   if (l1.val < l2.val) {
      l1.next = mergeTwoLists(l1.next, l2);
@@ -45,7 +44,25 @@ Both l1 and l2 are sorted in non-decreasing order.
       l2.next = mergeTwoLists(l1, l2.next);
       return l2;
     }
+
  };
 
  console.log(
    mergeTwoLists({val:1, next:{val:2, next:{val:4, next: null}}}, {val:1, next:{val:3, next: {val:4, next: null}}})) //[1,1,2,3,4,4]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
